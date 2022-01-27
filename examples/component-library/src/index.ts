a16046964f4ff9b42f5877164617b0e06d5c7d0e
Lukas Bombach <lukas.bombach@googlemail.com>
2022-01-27 14:26:34 +0100

    made the component library work

--- examples/component-library/src/index.ts
+++ examples/component-library/src/index.ts
@@ -1,1 +1,3 @@
+import "./tailwind.css";
+
 export * from "./Quote";
--- examples/next/pages/_app.tsx
+++ examples/next/pages/_app.tsx
@@ -1,3 +1,4 @@
+import "component-library/build/index.css";
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
